637061a268ec91fea715518a05fcee1afe7259d0
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-05-10 01:31:13 +0100

    Create fragments.ts

---
+++ src/lib/fragments.ts
@@ -0,0 +1,14 @@
+export const CDN = "https://psibadkdncspgikzzmnu.supabase.co/storage/v1/object/public/Fragments";
+
+export const FRAGMENTS: Record<string, { name: string; file: string }> = {
+  sword: { name: "Sword", file: "sword.png" },
+  helm: { name: "Helm", file: "helm.png" },
+  plate: { name: "Armor", file: "amor.png" },
+  gloves: { name: "Gloves", file: "gloves.png" },
+  boots: { name: "Boots", file: "boots.png" },
+  doom_potion: { name: "Doom Potion", file: "potion.png" },
+  book_of_reincarnation: { name: "Book of Reincarnation", file: "book-of-reincarnation.png" },
+  dragon_heart: { name: "Dragon Heart", file: "dragon-heart.png" },
+};
+
+export const FRAGMENT_TYPES = Object.keys(FRAGMENTS);
