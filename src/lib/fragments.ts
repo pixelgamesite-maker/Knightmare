b86abcc254e0046d195818a19dd4c77fded38737
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-05-12 21:22:25 +0100

    Update fragments.ts

--- src/lib/fragments.ts
+++ src/lib/fragments.ts
@@ -1,14 +1,12 @@
 export const CDN = "https://psibadkdncspgikzzmnu.supabase.co/storage/v1/object/public/Fragments";
 
 export const FRAGMENTS: Record<string, { name: string; file: string }> = {
-  sword: { name: "Sword", file: "sword.png" },
-  helm: { name: "Helm", file: "helm.png" },
-  plate: { name: "Armor", file: "armor.png" },
-  gloves: { name: "Gloves", file: "gloves.png" },
-  boots: { name: "Boots", file: "boots.png" },
-  doom_potion: { name: "Doom Potion", file: "potion.png" },
-  book_of_reincarnation: { name: "Book of Reincarnation", file: "book-of-reincarnation.png" },
-  dragon_heart: { name: "Dragon Heart", file: "dragon-heart.png" },
+  sword:                   { name: "Sword Fragment", file: "sword.png" },
+  helm:                    { name: "Helm Fragment", file: "helm.png" },
+  armor:                   { name: "Armor Fragment", file: "armor.png" },
+  gloves:                  { name: "Gloves Fragment", file: "gloves.png" },
+  boots:                   { name: "Boots Fragment", file: "boots.png" },
+  potion:                  { name: "Doom Potion", file: "potion.png" },
+  "book-of-reincarnation": { name: "Book of Reincarnation", file: "book-of-reincarnation.png" },
+  // dragon_heart REMOVED
 };
-
-export const FRAGMENT_TYPES = Object.keys(FRAGMENTS);
